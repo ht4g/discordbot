82991c75f3bb1117fcc60ddcfdcbb8298befeb87
Nguyễn Trung Hiếu <119673295+ht4g@users.noreply.github.com>
2023-05-22 19:42:13 +0700

    Add files via upload

---
+++ bot/commands/Info/help.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'help', 
+    category: 'info',
+    aliases: ['p'],
+    run: (client, message, args) => {
+        message.reply("**Layer7**\n`If you need help, contact overkill#4176`\n`or use your head /`");
+    }
+}
