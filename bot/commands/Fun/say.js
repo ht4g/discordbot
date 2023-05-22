1970b6612b7dcf37746ff2aa0c813b11cb58445e
Nguyễn Trung Hiếu <119673295+ht4g@users.noreply.github.com>
2023-05-22 19:42:51 +0700

    Delete bot/commands directory

--- bot/commands/Fun/say.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'say', 
-    category: 'Fun',
-    run: (client, message, args) => {
-        if (message.deletable) message.delete();
-        message.channel.send(args.join(' '));
-    }
-}
--- bot/commands/Info/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping', 
-    category: 'info',
-    aliases: ['p'],
-    run: (client, message, args) => {
-        message.reply(`🏓 Pong! \`${client.ws.ping}ms\``);
-    }
-}
